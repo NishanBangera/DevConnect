8d495a2dd1e66a33fd8e5c78758901419382aae7
Nishan Bangera <nishanbangera4321@gmail.com>
2025-12-11 22:18:57 +0530

    feat(frontend): implement LogIn page and update routing

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -4,17 +4,21 @@ import Home from './pages/Home'
 import Profile from './pages/Profile'
 import Settings from './pages/Settings'
 import NotFound from './pages/NotFound'
+import LogIn from './pages/LogIn'
 
 function App() {
   return (
-    <div>
+    <div className='flex flex-col h-full'>
       <NavBar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/settings" element={<Settings />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+      <div className="flex grow">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path='/login' element={<LogIn />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/settings" element={<Settings />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </div>
     </div>
   )
 }
